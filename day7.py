70605e3dc894a756959eeb48a64e630b15b63949
Akash-5675 <99992480+Akash-5675@users.noreply.github.com>
2024-10-30 16:25:23 +0530

    first commit

--- day7.py
+++ day7.py
@@ -2,14 +2,42 @@ import random
 word_list = ["aardvark" , "baboon" , "camel"]
 
 choosenWord = random.choice(word_list)
-
 print(choosenWord)
+
+lives = 6
+placeholder = ""
  
- 
-guess = input("Guess a letter in the word ").lower()
-print(guess)
-for i in choosenWord:
-    if i == guess:
-        print("Right")
-    else:
-        print("Wrong")
+wordLength = len(choosenWord)
+for i in range(wordLength):
+    placeholder += "_" 
+print(placeholder)
+
+gameover = False
+correctList = []
+
+while not gameover:
+    guess = input("Guess a letter in the word ").lower()
+    display = ""
+
+
+    for i in choosenWord:
+        if i == guess:
+            display += guess
+            correctList.append(guess)
+        elif i in correctList:
+            display += i
+        else:
+            display += "_"
+    
+    print(display)
+    
+    
+    while guess not in choosenWord:
+        lives -= 1
+        if lives == 0:
+            print("you lose")
+    
+    if "_" not in display:
+        gameover = True
+        print("you win!!!")
+
---
+++ day8.py
@@ -0,0 +1,4 @@
+def greet():
+    print("hi")
+    print("I am")
+    print("Akash")
